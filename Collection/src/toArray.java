9c78e57b4a7b3858630d3e4d2b1446a42adb11bf
avishakr kotkar <kotkaravi07@gmail.com>
2023-08-06 01:54:41 +0530

    Loops updated

--- Collection/src/toArray.java
+++ Collection/src/toArray.java
@@ -6,6 +6,7 @@ public class toArray {
     public static void main(String[] args) {
         ArrayList al = new ArrayList();
 
+
         al.add(10);
         al.add(20);
         al.add("ABC");
---
+++ Loops/src/program1.java
@@ -0,0 +1,4 @@
+public class program1 {
+    public static void main(String[] args) {
+    }
+}
