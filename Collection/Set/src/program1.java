eaa8bcab0d0400607c31e5aaf41364f481efac79
avishakr kotkar <kotkaravi07@gmail.com>
2023-08-09 23:57:00 +0530

    Collection set programs updated

---
+++ Collection/Set/src/program1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class program1 {
+    public static void main(String[] args) {
+
+
+            HashSet hs = new HashSet();
+
+            hs.add(10);
+            hs.add(20);
+            hs.add(10);
+            hs.add(20);
+
+            System.out.println(hs);
+        }
+}
