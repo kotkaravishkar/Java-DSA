60bdca2576638512367cdda292513a474d15ade7
avishakr kotkar <kotkaravi07@gmail.com>
2023-09-10 23:49:18 +0530

    Innerclass4

---
+++ InnerClass/src/InnerClass4.java
@@ -0,0 +1,30 @@
+public class InnerClass4 {
+    public static void main(String[] args) {
+
+        Demo1 obj = new Demo1();
+        obj.m1();
+        obj.m2();
+    }
+}
+
+class Demo1{
+
+    void m1(){
+
+        System.out.println("Core2Web");
+
+        class Inner{
+
+            void m3(){
+
+                System.out.println("Java");
+            }
+        }
+        Inner obj1 = new Inner();
+        obj1.m3();
+    }
+    void m2(){
+
+        System.out.println("C2W");
+    }
+}
