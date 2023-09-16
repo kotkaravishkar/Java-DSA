35e7e0f43aa5e9fb180eb7736cf33eaf5adf2dff
avishakr kotkar <kotkaravi07@gmail.com>
2023-09-16 22:43:17 +0530

    IpAddress2

---
+++ Networking/src/IpAddress2.java
@@ -0,0 +1,16 @@
+
+import java.net.*;
+import java.io.*;
+
+public class IpAddress2 {
+    public static void main(String[] args)throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String site = br.readLine();
+
+        InetAddress ip = InetAddress.getByName(site);
+
+        System.out.println("IP Address = " + ip);
+    }
+}
