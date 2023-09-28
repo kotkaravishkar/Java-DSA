777773569c8c1ad9d2f38a7c17e29fce1bcaf473
avishakr kotkar <kotkaravi07@gmail.com>
2023-09-28 23:55:23 +0530

    Server Connection

---
+++ Networking/src/server.java
@@ -0,0 +1,24 @@
+// Server Side Code :
+
+import java.net.*;
+import java.io.*;
+
+public class server {
+    public static void main(String[] args) throws IOException{
+
+        ServerSocket ss = new ServerSocket(1200);
+
+        Socket s = ss.accept();
+
+        System.out.println("Connection is established");
+
+        OutputStream os = s.getOutputStream();
+        PrintStream ps = new PrintStream(os);
+
+        ps.println("Hello Client");
+
+        ps.close();
+        os.close();
+        ss.close();
+    }
+}
